caea70d357413f10f7233c2ce4396ec04020b974
infolearnview-alt <infolearnview@gmail.com>
2026-06-03 17:06:43 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-window.LEARNVIEW_APPS_SCRIPT_URL = "";
+window.LEARNVIEW_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwdso6OvY2FAPI8N6ASxMqGrON1hmVYUmx4ZTFK7qU__odWZk_G7ASPxaFT89U9GY9zyg/exec";
